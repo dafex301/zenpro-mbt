4b6a05470dac3790838fd340b0862f29c18f1fd5
Fahrel <fahrel@students.undip.ac.id>
2023-10-02 22:56:57 +0700

    feat: profile state done

--- ongoing.state.js
+++ ongoing.state.js
@@ -11,56 +11,133 @@ const machine = createMachine(
         on: {
           HOME: {
             target: "#authenticated.homePage",
-            cond: "isNotPracticePage",
+            cond: "canAccessNavbar",
           },
           PROFILE: {
             target: "#authenticated.profilePage",
-            cond: "isNotPracticePage",
+            cond: "canAccessNavbar",
           },
           MATERI: {
             target: "#authenticated.materiPage",
-            cond: "isNotPracticePage",
+            cond: "canAccessNavbar",
           },
           VALID_SEARCH: {
             target: "#authenticated.searchPage.validSearch",
-            cond: "isNotPracticePage",
+            cond: "canAccessNavbar",
           },
           INVALID_SEARCH: {
             target: "#authenticated.searchPage.invalidSearch",
-            cond: "isNotPracticePage",
+            cond: "canAccessNavbar",
           },
         },
         states: {
-          homePage: {},
+          homePage: {
+            on: {
+              SELECT_PELATIHAN: "materiPage",
+            },
+          },
           profilePage: {
             id: "profilePage",
             on: {
-              VIEW_ACCOUNT: "#profilePage.viewAccountPage",
+              VIEW_ACCOUNT: "#profilePage.accountPage",
               PASSWORD: "#profilePage.passwordPage",
               SUBSCRIPTION: "#profilePage.subscriptionPage",
               LOGOUT: "#notAuthenticated",
             },
-            initial: "viewAccountPage",
+            initial: "accountPage",
             states: {
-              viewAccountPage: {
-                on: {
-                  EDIT_ACCOUNT: "editAccountPage",
+              accountPage: {
+                id: "accountPage",
+                initial: "viewAccountPage",
+                states: {
+                  viewAccountPage: {
+                    on: {
+                      EDIT_ACCOUNT: "#accountPage.editAccountPage",
+                    },
+                  },
+                  editAccountPage: {
+                    on: {
+                      CANCEL_EDIT_ACCOUNT: "#accountPage",
+                      SAVE_EDIT_ACCOUNT: "savingAccount",
+                    },
+                  },
+                  savingAccount: {
+                    on: {
+                      SAVING_ACCOUNT: "#accountPage",
+                    },
+                  },
                 },
               },
-              editAccountPage: {
-                on: {
-                  CANCEL_EDIT_ACCOUNT: "viewAccountPage",
-                  SAVE_EDIT_ACCOUNT: "savingAccount",
+              passwordPage: {
+                initial: "viewPasswordPage",
+                states: {
+                  viewPasswordPage: {
+                    on: {
+                      EDIT_PASSWORD: "editPasswordPage",
+                    },
+                  },
+                  editPasswordPage: {
+                    on: {
+                      SAVE_PASSWORD: "savePassword",
+                    },
+                  },
+                  savePassword: {
+                    on: {
+                      PASSWORD_SAVED: "viewPasswordPage",
+                    },
+                  },
                 },
               },
-              savingAccount: {
+              subscriptionPage: {
                 on: {
-                  SAVING_ACCOUNT: "viewAccountPage",
+                  BUY_SUBSCRIPTION: "#subscriptionModal",
                 },
               },
-              passwordPage: {},
-              subscriptionPage: {},
+              subscriptionModal: {
+                id: "subscriptionModal",
+                on: {
+                  ESCAPE: "subscriptionPage",
+                  CLOSE_MODAL: "subscriptionPage",
+                },
+                initial: "subs90",
+                states: {
+                  subsFree: {
+                    on: {
+                      SUBS_90: "subs90",
+                      SUBS_180: "subs180",
+                      SUBS_365: "subs365",
+                    },
+                  },
+                  subs90: {
+                    on: {
+                      SUBS_FREE: "subsFree",
+                      SUBS_180: "subs180",
+                      SUBS_365: "subs365",
+                      BUY_SUBS_BTN: "#subsPaymentPage",
+                    },
+                  },
+                  subs180: {
+                    on: {
+                      SUBS_FREE: "subsFree",
+                      SUBS_90: "subs90",
+                      SUBS_365: "subs365",
+                      BUY_SUBS_BTN: "#subsPaymentPage",
+                    },
+                  },
+                  subs365: {
+                    on: {
+                      SUBS_FREE: "subsFree",
+                      SUBS_90: "subs90",
+                      SUBS_180: "subs180",
+                      BUY_SUBS_BTN: "#subsPaymentPage",
+                    },
+                  },
+                },
+              },
             },
+          },
+          subsPaymentPage: {
+            id: "subsPaymentPage",
           },
           materiPage: {
             on: {
@@ -134,8 +211,11 @@ const machine = createMachine(
   },
   {
     guards: {
-      isNotPracticePage: (context, event, { state }) => {
-        return !state.matches("authenticated.practicePage.ongoing");
+      canAccessNavbar: (context, event, { state }) => {
+        return (
+          !state.matches("authenticated.profilePage.subscriptionModal") &&
+          !state.matches("authenticated.practicePage.ongoing")
+        );
       },
     },
   }
